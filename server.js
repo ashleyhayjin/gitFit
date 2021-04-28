c0cc9cb14b5c7469c622e2774fb5164fa3c4abd2
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-04-28 10:42:01 -0500

    PORT number

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(express.static("public"));
 app.use(homeRoutes);
 app.use(apiRoutes);
 
+const PORT = process.env.PORT || 3001;
+
 mongConnect.once("open", () => {
-  app.listen(3000, function() {
+  app.listen(PORT, function() {
     console.log("Server started on port 3000");
   });
 })
